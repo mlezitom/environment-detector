d787dc8d90b26d22ed5295d10deb3b2f81e0e467
Tomas Mleziva <tomas@mleziva.eu>
2014-12-09 17:06:18 +0100

    Main class

---
+++ src/EnvironmentDetector.php
@@ -0,0 +1,72 @@
+<?php
+use Nette\DI\Container;
+
+/**
+ * Description of EnvironmentDetector
+ *
+ * @author Tomáš
+ */
+class EnvironmentDetector {
+    
+    public $data;
+    public $domain;
+    public $ip;
+    
+    const SECTION_PRODUCTION = "production";
+    const SECTION_DEVELOPMENT = "development";
+    const SECTION_LOCALHOST = "localhost";
+    
+    public function __construct() {
+        $this->domain = $_SERVER['HTTP_HOST'];
+        $this->ip = $_SERVER['REMOTE_ADDR'];
+    }
+    
+    public function loadNeon($neonFile) {
+        if(!is_file($neonFile)) {
+            throw new \Nette\Application\ApplicationException(sprintf("Environment configuration file '%s' not found.", $neonFile));
+        }
+        
+        $this->data = Nette\Neon\Neon::decode(file_get_contents($neonFile));
+    }
+    
+    public function isDevel() {
+        return $this->is(self::SECTION_DEVELOPMENT);
+    }
+    
+    public function isProduction() {
+        return $this->is(self::SECTION_PRODUCTION);
+    }
+    
+    public function isLocalhost() {
+        return $this->is(self::SECTION_LOCALHOST);
+    }
+    
+    private function is($mode) {
+        if(is_string($this->data[$mode])) {
+            return ($this->domain == $this->data[$mode] || $this->ip == $this->data[$mode]);
+        }
+        
+        if(is_array($this->data[$mode])) {
+            foreach($this->data[$mode] as $item) {
+                if($this->domain == $item || $this->ip == $item) {
+                    return TRUE;
+                }
+            }
+            return ($this->domain == $this->data[$mode]);
+        }
+        else {
+            throw new \Nette\InvalidArgumentException("Invalid data in evironment conf file - section " . $mode);
+        }
+        return false;
+    }
+    
+    public function getMode() {
+        if($this->isLocalhost()) {
+            return self::SECTION_LOCALHOST;
+        }
+        if($this->isDevel()) {
+            return self::SECTION_DEVELOPMENT;
+        }
+        return self::SECTION_PRODUCTION;
+    }
+}
