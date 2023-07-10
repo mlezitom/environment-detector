6a261115975d40796323115fc9ba51a7eff3c54b
Mlezitom <mlezivat@gmail.com>
2023-07-10 13:25:05 +0200

    PHP8 update

--- src/EnvironmentDetector.php
+++ src/EnvironmentDetector.php
@@ -14,7 +14,8 @@ class EnvironmentDetector {
     const SECTION_DEVELOPMENT = "development";
     const SECTION_LOCALHOST = "localhost";
     
-    public function __construct($neon = NULL) {
+    public function __construct($neon = NULL)
+    {
         $this->domain = $_SERVER['HTTP_HOST'];
         $this->ip = $_SERVER['REMOTE_ADDR'];
 		if($neon) {
@@ -22,46 +23,52 @@ class EnvironmentDetector {
 		}
     }
     
-    public function loadNeon($neonFile) {
+    public function loadNeon($neonFile)
+    {
         if(!is_file($neonFile)) {
-            throw new \Nette\Application\ApplicationException(sprintf("Environment configuration file '%s' not found.", $neonFile));
+            throw new \Exception(sprintf("Environment configuration file '%s' not found.", $neonFile));
         }
         
         $this->data = Nette\Neon\Neon::decode(file_get_contents($neonFile));
     }
     
-    public function isDevel() {
+    public function isDevel()
+    {
         return $this->is(self::SECTION_DEVELOPMENT);
     }
     
-    public function isProduction() {
+    public function isProduction()
+    {
         return $this->is(self::SECTION_PRODUCTION);
     }
     
-    public function isLocalhost() {
+    public function isLocalhost()
+    {
         return $this->is(self::SECTION_LOCALHOST);
     }
     
-    private function is($mode) {
+    private function is($mode)
+    {
         if(is_string($this->data[$mode])) {
-            return ($this->domain == $this->data[$mode] || $this->ip == $this->data[$mode]);
+            return (str_contains($this->domain, $this->data[$mode]) || $this->ip == $this->data[$mode]);
         }
-        
+
         if(is_array($this->data[$mode])) {
             foreach($this->data[$mode] as $item) {
-                if($this->domain == $item || $this->ip == $item) {
+                if(str_contains($item, $this->domain) || $this->ip == $item) {
                     return TRUE;
                 }
             }
-            return ($this->domain == $this->data[$mode]);
+            return (in_array($this->domain, $this->data[$mode]));
         }
         else {
-            throw new \Nette\InvalidArgumentException("Invalid data in evironment conf file - section " . $mode);
+            throw new InvalidArgumentException("Invalid data in environment conf file - section " . $mode);
         }
         return false;
     }
     
-    public function getMode() {
+    public function getMode()
+    {
         if($this->isLocalhost()) {
             return self::SECTION_LOCALHOST;
         }
